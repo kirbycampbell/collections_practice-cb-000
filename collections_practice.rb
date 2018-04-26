ef01faf652d04f2a6a94cf6d4e5d228ef11cc7fa
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-26 06:03:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 
+def sort_array_asc(int)
+  int.sort
+end
+
+def sort_array_desc(int)
+  int.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(int)
+  int.reverse
+end
+
+def kesha_maker(strings)
+  new_array = []
+  strings.each do |i|
+    i[2] = "$"
+    new_array << i
+  end
+  new_array
+end
+
+def find_a(array)
+  new_array = []
+  array.collect do |i|
+    if i.start_with?("a")
+     new_array << i
+   end
+  end
+  return new_array
+end
+
+def sum_array(array)
+  array.inject(0){|sum, x| sum + x}
+end
+
+def add_s(array)
+  new_array = []
+  array.each do |i|
+    if i == array[1]
+      next
+    else
+        i.insert(-1, "s")
+        new_array << i
+    end
+    new_array
+  end
+end
