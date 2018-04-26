e9d6a2b16557d4708f48f2676d326206d9bb740c
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-26 05:25:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 
+def sort_array_asc(int)
+  int.sort
+end
+
+def sort_array_desc(int)
+  int.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort.length
+end
+
+def swap_elements(strings)
+
+end
